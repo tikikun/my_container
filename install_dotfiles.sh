c37144a596ba889853fbfff971ab90ed24f17bd3
tikikun <daogiatuank54@gmail.com>
2023-09-11 07:59:00 +0700

    check condition also

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+set -e
+
+trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exception occurred!"' EXIT
+
 mkdir -p ~/.config/nvim
 cp -r ./vim_setup/* ~/.config/nvim
 cp -r dotfiles/. ~/
