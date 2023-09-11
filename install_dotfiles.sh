da0f26e5984933b917c8ca29365e733d1482f410
tikikun <daogiatuank54@gmail.com>
2023-09-11 08:00:57 +0700

    force replace in case there is already dotfiles

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,5 +5,5 @@ set -e
 trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exception occurred!"' EXIT
 
 mkdir -p ~/.config/nvim
-cp -r ./vim_setup/* ~/.config/nvim
-cp -r dotfiles/. ~/
+cp -rf ./vim_setup/* ~/.config/nvim
+cp -rf dotfiles/. ~/
