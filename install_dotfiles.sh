e63d4b59363b7788f34674a1033738b0788f1842
Alan Dao <contact@daogiatuan.com>
2023-10-04 22:18:04 +0700

    correct way to install

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,7 +4,7 @@ set -e
 
 trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exception occurred!"' EXIT
 
-mkdir -p ~/.config/nvim
+# mkdir -p ~/.config/nvim
 ln -sf $(pwd)/vim_setup ~/.config/nvim
 ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
