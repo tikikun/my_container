21105be7739bb5396fb03138cc18ef3997e6c861
tikikun <daogiatuank54@gmail.com>
2024-04-30 14:27:25 +0700

    exit if not empty

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,7 +5,13 @@ set -e
 trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exception occurred!"' EXIT
 
 # mkdir -p ~/.config/nvim
-ln -sf $(pwd)/vim_setup ~/.config/nvim
-ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
-ln -sf $(pwd)/../openai_key ~/openai_key
+if [ -d ~/.config/nvim ] && [ -z "$(ls -A ~/.config/nvim)" ]; then
+    echo "Directory is empty. Proceeding with setup..."
+    # mkdir -p ~/.config/nvim # Uncomment if there's any possibility the directory doesn't exist.
+    ln -sf $(pwd)/vim_setup ~/.config/nvim
+    ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
+    ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
+    ln -sf $(pwd)/../openai_key ~/openai_key
+else
+    echo "Directory is not empty or does not exist. Aborting setup."
+fi
