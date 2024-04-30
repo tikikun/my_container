9df9237de9d9680fbe1e5a9dbfcbbb07f773ba10
tikikun <daogiatuank54@gmail.com>
2024-04-30 14:11:59 +0700

    install openai key

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -8,3 +8,4 @@ trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exce
 ln -sf $(pwd)/vim_setup ~/.config/nvim
 ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
+ln -sf $(pwd)/../openai_key.sh ~/openai_key.sh
