0822791a0e70d4f425c0beeb0ede789426f20d4f
tikikun <daogiatuank54@gmail.com>
2024-05-05 10:05:15 +0700

    no ned to create dir

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,7 +7,6 @@ trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exce
 # Ensure the directory does not exist before proceeding.
 if [ ! -d ~/.config/nvim ]; then
     echo "nvim directory does not exist. Proceeding with setup..."
-    mkdir -p ~/.config/nvim # This will create the directory if it does not exist.
     ln -sf $(pwd)/vim_setup ~/.config/nvim
     ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
     ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
