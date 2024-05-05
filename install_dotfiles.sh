ed2ed9837902e3755a960189486d8275ab0668c2
tikikun <daogiatuank54@gmail.com>
2024-05-05 10:03:33 +0700

    do it when dir not exist

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,14 +4,15 @@ set -e
 
 trap '[[ $? -eq 0 ]] && echo "All commands executed successfully!" || echo "Exception occurred!"' EXIT
 
-# mkdir -p ~/.config/nvim
-if [ -d ~/.config/nvim ] && [ -z "$(ls -A ~/.config/nvim)" ]; then
-    echo "Directory is empty. Proceeding with setup..."
-    # mkdir -p ~/.config/nvim # Uncomment if there's any possibility the directory doesn't exist.
+# Ensure the directory does not exist before proceeding.
+if [ ! -d ~/.config/nvim ]; then
+    echo "nvim directory does not exist. Proceeding with setup..."
+    mkdir -p ~/.config/nvim # This will create the directory if it does not exist.
     ln -sf $(pwd)/vim_setup ~/.config/nvim
     ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
     ln -sf $(pwd)/dotfiles/.zshrc ~/.zshrc
     ln -sf $(pwd)/../openai_key ~/openai_key
 else
-    echo "Directory is not empty or does not exist. Aborting setup."
+    echo "nvim directory already exists. Aborting setup."
 fi
+
